bc7d68054fd22e81429c86a4c8110fce13ee52bf
jasser <jasserj663@gmail.com>
2025-02-22 00:45:27 +0100

    intercepting POST req comming from the consumer

--- llm/interface_fun/gemini_interface_func.py
+++ llm/interface_fun/gemini_interface_func.py
@@ -7,10 +7,8 @@ sys_instruct="return json object"
 # Create the model
 generation_config = {
   "temperature": 1,
-  "top_p": 0.95,
-  "top_k": 40,
   "max_output_tokens": 8192,
-  "response_mime_type": "text/plain",
+  "response_mime_type": "application/json",
 }
 
 model = genai.GenerativeModel(
@@ -19,22 +17,17 @@ model = genai.GenerativeModel(
   generation_config=generation_config,
 )
 
-def clean_json_string(json_str):
-    # Remove markdown code block markers and any newlines at the start
-    json_str = re.sub(r'^```json\s*\n', '', json_str)
-    json_str = re.sub(r'\n```$', '', json_str)
-    return json_str.strip()
 
 def correct_api(api,api_schema):
 
   prompt = f"""
-  add to this api random inforamtions keep the old ones and add on it:
+  add to this api random inforamtions keep the old ones and add on it add at least 6 more fields:
   api = {api}
   """
 
   chat_session = model.start_chat()
   response = chat_session.send_message(prompt)
-  cleaned_json=clean_json_string(response.text)
+  cleaned_json=response.text
   return json.loads(cleaned_json)
 
 #num_tokens=model.count_tokens([prompt,response.text]) # Count the number of tokens in the response and in the prompt
